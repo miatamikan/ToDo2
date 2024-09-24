628ae42a1be4411f7064267637aebadb58bdd06b
miatamikan <viridian@yo-mu.sakura.ne.jp>
2024-09-24 21:16:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import configparser
 
 app = Flask(__name__)
 
-# config.ini から設定を読み込む
+# config.ini の読み込み
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 # IDとパスワード、シークレットキーを設定ファイルから取得
-USER_ID = config['DEFAULT'].get('USER_ID', 'admin')  # デフォルトIDは 'admin'
-PASSWORD = config['DEFAULT'].get('PASSWORD', '1111')  # デフォルトパスワードは '1111'
-app.secret_key = config['DEFAULT'].get('SECRET_KEY', 'your_secret_key')
+USER_ID = config['DEFAULT'].get('USER_ID')  # デフォルトのUSER_IDを取得
+PASSWORD = config['DEFAULT'].get('PASSWORD')  # パスワードも文字列として取得
+app.secret_key = config['DEFAULT'].get('SECRET_KEY')
 
 # ログインページ
 @app.route('/login', methods=['GET', 'POST'])
@@ -19,7 +19,7 @@ def login():
         input_user_id = request.form['user_id']
         input_password = request.form['password']
 
-        # 設定ファイルから読み込んだIDとパスワードと照合
+        # IDとパスワードの照合
         if input_user_id == USER_ID and input_password == PASSWORD:
             session['logged_in'] = True
             flash('ログインに成功しました！')
